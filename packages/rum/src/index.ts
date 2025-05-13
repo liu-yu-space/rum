aa7bbf137c926680669e0b7f347a1c67e7cca7cc
wb_wangyanle <wb_wangyanle@meituan.com>
2025-05-13 17:07:40 +0800

    feat: 添加类型导出，增强模块的类型定义支持

--- packages/rum/src/index.ts
+++ packages/rum/src/index.ts
@@ -2,3 +2,9 @@ export { request } from "./request";
 // 未来可扩展
 // export { SocketManager } from "./socket/SocketManager";
 // export { SSEManager } from "./sse/SSEManager";
+export type {
+  Method,
+  RequestConfig,
+  RequestInterceptor,
+  ResponseInterceptor,
+} from "./types";
